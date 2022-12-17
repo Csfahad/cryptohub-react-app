f63e8a610a8988eb9f59324d85a4e73f39f06f84
Csfahad <76874725+Csfahad@users.noreply.github.com>
2022-12-17 17:26:26 +0530

    minor bug fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-      <HStack p={"4"} shadow={"md"} justifyContent={['center', 'space-evenly']} width={'full'}>
+      <HStack p={"4"} shadow={['none', "md"]} justifyContent={['center', 'space-evenly']} width={'full'}>
         
-        <Button variant={'unstyled'}>
+        <Button variant={'ghost'}>
           <Link to="/">Home</Link>
         </Button>
-        <Button variant={"unstyled"}>
+        <Button variant={"ghost"}>
           <Link to="/exchanges">Exchanges</Link>
         </Button>
-        <Button variant={'unstyled'}>
+        <Button variant={'ghost'}>
           <Link to="/coins">Coins</Link>
         </Button>
       </HStack>
